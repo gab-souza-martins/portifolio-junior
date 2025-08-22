21455eb18c9260f1d414dfe87b92a90a849d7fd0
Grey-B <gabriellemener@gmail.com>
2025-08-22 15:22:55 -0300

    Criada validação dos inputs para email, incluindo mensagem de aviso e formatação dos inputs quando inválidos

--- js/emailForm.js
+++ js/emailForm.js
@@ -1,8 +1,40 @@
 var emailAssunto = document.getElementById("emailAssunto");
 var emailConteudo = document.getElementById("emailConteudo");
 var emailSubmitBtn = document.getElementById("emailSubmitBtn");
+var assuntoAviso = document.getElementById("assuntoAviso");
+var conteudoAviso = document.getElementById("conteudoAviso");
 var assunto;
 var conteudo;
+var inputsValidos = false;
+function validarInputs() {
+    if (emailAssunto.value === "") {
+        emailAssunto.classList.add("border-danger");
+        emailAssunto.classList.add("focus-ring");
+        emailAssunto.classList.add("focus-ring-danger");
+        assuntoAviso.classList.remove("d-none");
+    }
+    else {
+        emailAssunto.classList.remove("border-danger");
+        emailAssunto.classList.remove("focus-ring");
+        emailAssunto.classList.remove("focus-ring-danger");
+        assuntoAviso.classList.add("d-none");
+    }
+    if (emailConteudo.value === "") {
+        emailConteudo.classList.add("border-danger");
+        emailConteudo.classList.add("focus-ring");
+        emailConteudo.classList.add("focus-ring-danger");
+        conteudoAviso.classList.remove("d-none");
+    }
+    else {
+        emailConteudo.classList.remove("border-danger");
+        emailConteudo.classList.remove("focus-ring");
+        emailConteudo.classList.remove("focus-ring-danger");
+        conteudoAviso.classList.add("d-none");
+    }
+    if (emailAssunto.value !== "" && emailConteudo.value !== "") {
+        inputsValidos = true;
+    }
+}
 function enviarEmail() {
     if (emailAssunto) {
         assunto = emailAssunto.value;
@@ -14,5 +46,8 @@ function enviarEmail() {
 }
 emailSubmitBtn.addEventListener("click", function (e) {
     e.preventDefault();
-    enviarEmail();
+    validarInputs();
+    if (inputsValidos) {
+        enviarEmail();
+    }
 });
--- ts/emailForm.ts
+++ ts/emailForm.ts
@@ -8,8 +8,50 @@ const emailSubmitBtn = document.getElementById(
    "emailSubmitBtn"
 ) as HTMLElement | null;
 
+const assuntoAviso = document.getElementById(
+   "assuntoAviso"
+) as HTMLElement | null;
+const conteudoAviso = document.getElementById(
+   "conteudoAviso"
+) as HTMLElement | null;
+
 let assunto: string;
 let conteudo: string;
+let inputsValidos: boolean = false;
+
+function validarInputs() {
+   if (emailAssunto.value === "") {
+      emailAssunto.classList.add("border-danger");
+      emailAssunto.classList.add("focus-ring");
+      emailAssunto.classList.add("focus-ring-danger");
+
+      assuntoAviso.classList.remove("d-none");
+   } else {
+      emailAssunto.classList.remove("border-danger");
+      emailAssunto.classList.remove("focus-ring");
+      emailAssunto.classList.remove("focus-ring-danger");
+
+      assuntoAviso.classList.add("d-none");
+   }
+
+   if (emailConteudo.value === "") {
+      emailConteudo.classList.add("border-danger");
+      emailConteudo.classList.add("focus-ring");
+      emailConteudo.classList.add("focus-ring-danger");
+
+      conteudoAviso.classList.remove("d-none");
+   } else {
+      emailConteudo.classList.remove("border-danger");
+      emailConteudo.classList.remove("focus-ring");
+      emailConteudo.classList.remove("focus-ring-danger");
+
+      conteudoAviso.classList.add("d-none");
+   }
+
+   if (emailAssunto.value !== "" && emailConteudo.value !== "") {
+      inputsValidos = true;
+   }
+}
 
 function enviarEmail() {
    if (emailAssunto) {
@@ -22,7 +64,11 @@ function enviarEmail() {
    window.location.href = `mailto:gabriellemener@gmail.com?subject=${assunto} &body=${conteudo}`;
 }
 
-emailSubmitBtn.addEventListener("click", (e) => {
+emailSubmitBtn.addEventListener("click", (e: MouseEvent) => {
    e.preventDefault();
-   enviarEmail();
+
+   validarInputs();
+   if (inputsValidos) {
+      enviarEmail();
+   }
 });
